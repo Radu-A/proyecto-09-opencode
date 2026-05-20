5e0472abc9e11833b3330f4d3f5db9c50bce683d
Radu-A <elcastravete@gmail.com>
2026-05-20 11:57:56 +0200

    chore: create file structure

---
+++ src/main/java/com/github/Radu_A/proyecto_09/Proyecto09Application.java
@@ -0,0 +1,13 @@
+package com.github.Radu_A.proyecto_09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto09Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Radu_A/proyecto_09/Proyecto09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Radu_A.proyecto_09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
